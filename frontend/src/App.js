b9820d7da6c03072206d3bfd33605484e2ba363c
Adoilson Freitas <adoilsonfreitas@gmail.com>
2020-03-25 21:12:25 -0300

    Frontend do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import './global.css'
+
+import Routes from './routes';
+
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Routes />
   );
 }
 
