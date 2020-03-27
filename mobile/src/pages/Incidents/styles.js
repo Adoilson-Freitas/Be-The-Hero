50c0b69cf49d7d3eeee25be4b53a7b4948f5c966
Adoilson Freitas <adoilsonfreitas@gmail.com>
2020-03-26 22:26:24 -0300

    mobile da aplicação

---
+++ mobile/src/pages/Incidents/styles.js
@@ -0,0 +1,76 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00001a',
+    paddingHorizontal: 24,
+    paddingTop:  Constants.statusBarHeight + 20,
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  }, 
+
+  headerText: {
+    fontSize: 15,
+    color: '#eee',
+  },
+
+  headerTextBold: {
+    fontWeight: 'bold',
+  },
+
+  title: {
+    fontSize: 30,
+    marginBottom: 16,
+    marginTop: 48,
+    color: '#FFF',
+    fontWeight: 'bold',
+  }, 
+
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#eee',
+  },
+
+  incidentList: {
+    marginTop: 32,
+  },
+
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#000033',
+    marginBottom: 16,
+  },
+
+  incidentProperty: {
+    fontSize: 14,
+    color: '#777',
+    fontWeight: 'bold',
+  },
+
+  incidentValue: {
+    fontSize: 15,
+    color: '#eee',
+    marginTop: 8,
+    marginBottom: 24,
+  },
+
+  detailsButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center', 
+  },
+
+  detailsButtonText: {
+   color: '#e02041',
+   fontSize: 15,
+   fontWeight: 'bold',
+  }
+});
